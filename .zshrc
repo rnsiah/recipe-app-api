33a2dc20db1d8d83921750d32d09c227fc58058b
Altrue <r.nsiah@gmail.com>
2024-02-05 10:46:51 -0600

    install postgresl database

---
+++ .zshrc
@@ -0,0 +1,13 @@
+
+
+
+source $HOME/.aliases
+
+export PATH=$PATH:/usr/local/bin/docker-compose
+
+export PATH=$PATH:/Users/kofi/Library/Developer/flutter/bin
+export PATH="/usr/local/anaconda3/bin:$PATH"
+
+PATH="/Library/Frameworks/Python.framework/Versions/3.9/bin:${PATH}"
+export PATH
+
---
+++ app/app/tests.py
@@ -0,0 +1,22 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module"""
+
+    def test_add_numbers(self):
+        """ Test adding numbers together"""
+        res = calc.add(4,5)
+
+        self.assertEqual(res, 9)
+
+    def test_subtract_numbers(self):
+        """Test subtracting numbers"""
+
+        res= calc.subtract(10, 15)
+        self.assertEqual(res, 5)
